f6fa9af4da95d6c0a44e8900f71095a183b83619
LeviMardis <85965360+LeviMardis@users.noreply.github.com>
2024-06-13 15:35:34 -0500

    test

--- dockline-wp-suite.php
+++ dockline-wp-suite.php
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('DOCKLINE_WP_SUITE_VERSION', '1.0.3');
+define('DOCKLINE_WP_SUITE_VERSION', '1.0.4');
 
 /**
  * The code that runs during plugin activation.
